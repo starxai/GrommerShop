cf56affd098ec20773350c8b9a2e98c0549cc900
StarXAi Enterprises <147140857+starxai@users.noreply.github.com>
2024-09-02 11:28:37 +0530

    added aws server

--- src/context/axiox.js
+++ src/context/axiox.js
@@ -2,6 +2,6 @@ const isLocal = false ;
 
 const Context = isLocal
   ? "http://127.0.0.1:8000"
-  : "https://groomerapp-e1f70ad38863.herokuapp.com";
+  : "http://groomerloadbalancer-1779385022.ap-south-1.elb.amazonaws.com/api/";
 
 export default Context;
